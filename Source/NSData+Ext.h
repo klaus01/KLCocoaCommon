2780436b98d1a46c77a655fe050488d26d4e9b89
klaus01 <kelei0017@gmail.com>
2013-03-07 00:01:57 +0800

    + NSData(Ext)

---
+++ Source/NSData+Ext.h
@@ -0,0 +1,16 @@
+//
+//  NSData+Ext.h
+//  KLCocoaCommon
+//
+//  Created by 柯磊 on 13-3-6.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (Ext)
+
+/// 返回当前数据的MD5码(大写)
+- (NSString *)md5;
+
+@end
