dcdef96fada4534725456e25f6ddb93c5c689606
klaus01 <kelei0017@gmail.com>
2013-03-07 22:46:38 +0800

    + 选中行的同时显示出该行

--- Source/UITableView+Ext.h
+++ Source/UITableView+Ext.h
@@ -10,4 +10,10 @@
 
 @interface UITableView (Ext)
 
+/// 选中indexPath后显示到UITableView的可视范围内
+/**
+ 根据indexPath的位置自动选择scrollPosition，使得选中indexPath后显示在UITableView的可视范围内
+ */
+- (void)selectRowAtIndexPath:(NSIndexPath *)indexPath animated:(BOOL)animated;
+
 @end
--- Source/UITableView+Ext.m
+++ Source/UITableView+Ext.m
@@ -10,4 +10,17 @@
 
 @implementation UITableView (Ext)
 
+- (void)selectRowAtIndexPath:(NSIndexPath *)indexPath animated:(BOOL)animated
+{
+    CGRect cellFrame = [self rectForRowAtIndexPath:indexPath];
+    UITableViewScrollPosition scrollPosition;
+    if (cellFrame.origin.y < self.contentOffset.y)
+        scrollPosition = UITableViewScrollPositionTop;
+    else if (CGRectGetMaxY(cellFrame) > (self.contentOffset.y + self.bounds.size.height))
+        scrollPosition = UITableViewScrollPositionBottom;
+    else
+        scrollPosition = UITableViewScrollPositionNone;
+    [self selectRowAtIndexPath:indexPath animated:animated scrollPosition:scrollPosition];
+}
+
 @end
