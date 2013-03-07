7a4b669689796669e1ee8ef6dc3b2694771cb9dd
klaus01 <kelei0017@gmail.com>
2013-03-07 22:44:16 +0800

    增加一些方便实例化UIColor的方法

--- Source/UIColor+Ext.h
+++ Source/UIColor+Ext.h
@@ -10,4 +10,15 @@
 
 @interface UIColor (Ext)
 
+/// 根据RBG值返回颜色
++ (UIColor *)colorWithR:(unsigned short)r G:(unsigned short)g B:(unsigned short)b;
++ (UIColor *)colorWithR:(unsigned short)r G:(unsigned short)g B:(unsigned short)b alpha:(CGFloat)alpha;
+
+/// R,G,B都使用相同的rgb值返回颜色
++ (UIColor *)colorWithRGB:(unsigned short)rgb;
++ (UIColor *)colorWithRGB:(unsigned short)rgb alpha:(CGFloat)alpha;
+
+/// 16进制字符串转颜色（支持：#RGB,#ARGB,#RRGGBB,#AARRGGBB，可不带#号）
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+
 @end
